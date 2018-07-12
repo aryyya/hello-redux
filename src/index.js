d95e1ef38970885916b38eddfe1f8003a8737483
aryyya <arya@live.ca>
2018-07-11 20:43:04 -0700

    Add redux-devtools-extension.

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import { createStore } from 'redux'
+import { createStore, applyMiddleware } from 'redux'
+import { composeWithDevTools } from 'redux-devtools-extension'
 import { Provider } from 'react-redux'
 import reducers from './redux/reducers'
 
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-const store = createStore(reducers)
+const store = createStore(reducers, composeWithDevTools(applyMiddleware()))
 
 ReactDOM.render(
   <Provider store={store}>
--- src/redux/actions/action-types.js
+++ src/redux/actions/action-types.js
@@ -9,4 +9,3 @@ export const VisibilityFilters = {
 // todos
 export const ADD_TODO = 'ADD_TODO'
 export const TOGGLE_TODO = 'TOGGLE_TODO'
-
