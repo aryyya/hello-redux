91f844c3c70230148571ba48e50f08fa84bd1f16
aryyya <arya@live.ca>
2018-07-14 19:47:10 -0700

    Add thunk middleware for async actions.

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,11 +1,14 @@
 import { createStore, applyMiddleware } from 'redux'
 import { composeWithDevTools } from 'redux-devtools-extension'
+import thunkMiddleware from 'redux-thunk'
 import reducers from './reducers'
 
 const store = createStore(
   reducers,
   composeWithDevTools(
-    applyMiddleware()
+    applyMiddleware(
+      thunkMiddleware
+    )
   )
 )
 
