bcde210d7d8a9502f2214bec1a5f9a088e99edf5
aryyya <arya@live.ca>
2018-07-19 21:17:23 -0700

    Persist state to localstorage and read it on load.

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -3,13 +3,20 @@ import { composeWithDevTools } from 'redux-devtools-extension'
 import thunkMiddleware from 'redux-thunk'
 import reducers from './reducers'
 
+const persistedState = localStorage.getItem('reduxState') ? JSON.parse(localStorage.getItem('reduxState')) : {}
+
 const store = createStore(
   reducers,
+  persistedState,
   composeWithDevTools(
     applyMiddleware(
       thunkMiddleware
     )
   )
 )
+
+store.subscribe(() => {
+  localStorage.setItem('reduxState', JSON.stringify(store.getState()))
+})
 
 export default store
