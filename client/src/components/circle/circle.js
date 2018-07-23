33ff28f3ef6c20dd4820fca1b0dcc817131e5964
aryyya <arya@live.ca>
2018-07-22 17:19:35 -0700

    Add colorful bullets.

---
+++ client/src/components/circle/circle.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './circle.css'
+
+const Circle = props => {
+  return (
+    <span className="circle"></span>
+  )
+}
+
+export default Circle
--- client/src/components/todo-item/todo-item.js
+++ client/src/components/todo-item/todo-item.js
@@ -2,13 +2,17 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import './todo-item.css'
 import Checkbox from '../checkbox/checkbox'
+import Circle from '../circle/circle'
 import TodoItemPanel from '../todo-item-panel/todo-item-panel'
 
 const TodoItem = ({ completed, text, id, toggleTodo, deleteTodo, selectTodo, selectedTodoId }) => {
   return (
     <li className={`todo-item ${completed ? 'todo-item--completed' : ''} ${id === selectedTodoId ? 'todo-item--selected' : ''}`}>
       <div className="todo-item__top">
-        <div className="todo-item__text">{text}</div>
+        <div className="todo-item__text-wrapper">
+          <Circle />
+          <span className="todo-item__text">{text}</span>
+        </div>
         <div className="todo-item__buttons">
           <Checkbox
             checked={completed}
