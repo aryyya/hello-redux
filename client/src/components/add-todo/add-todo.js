7fb8f7d40755f2a013a06037b2d99ab792142f85
aryyya <arya@live.ca>
2018-07-25 16:54:34 -0700

    Add new todo form.

--- client/src/components/add-todo/add-todo.js
+++ client/src/components/add-todo/add-todo.js
@@ -1,18 +1,70 @@
-import React from 'react'
+import React, { Component } from 'react'
+import './add-todo.css'
 import { Link } from 'react-router-dom'
+import { connect } from 'react-redux'
+import { todosActions } from '../../redux/todos';
 
-const AddTodo = props => {
-  return (
-    <div className="add-todo">
-      <Link
-        to="/"
-      >
-        Go back.
-      </Link>
-      <br />
-      This is the AddTodo component.
-    </div>
-  )
+class AddTodo extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      text: ''
+    }
+  }
+  addTodo (event) {
+    if (event) {
+      event.preventDefault()
+    }
+    this.props.addTodo(this.state.text)
+    this.props.history.push('/')
+  }
+  render () {
+    return (
+      <div className="add-todo">
+        <form
+          className="add-todo__form"
+          onSubmit={this.addTodo.bind(this)}
+        >
+          <div>
+            <Link
+              className="add-todo__close"
+              to="/"
+            >
+              <img
+                className="add-todo__close-icon"
+                src="close-icon.svg"
+                alt="Close."
+              />
+            </Link>
+          </div>
+          <div>
+            <input
+              className="add-todo__text"
+              autoFocus
+              type="text"
+              value={this.state.text}
+              onChange={event => this.setState({ text: event.target.value })}
+              placeholder="Write task here"
+            />
+          </div>
+        </form>
+        <div className="add-todo__buttons">
+          <div
+            className="add-todo__add-button"
+            onClick={this.addTodo.bind(this)}
+          >
+            Add Todo
+          </div>
+        </div>
+      </div>
+    )
+  }
 }
 
-export default AddTodo
+const mapDispatchToProps = dispatch => {
+  return {
+    addTodo: text => todosActions.addTodo(text)
+  }
+}
+
+export default connect(null, mapDispatchToProps)(AddTodo)
