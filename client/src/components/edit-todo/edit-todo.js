77ba86223c492c363cdd7071025080814aab9df5
aryyya <arya@live.ca>
2018-07-28 23:08:50 -0700

    Add edit-todo component.

---
+++ client/src/components/edit-todo/edit-todo.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './add-todo.css'
+
+const EditTodo = props => {
+  return (
+    <div className="edit-todo">
+      edit-todo
+    </div>
+  )
+}
+
+export default EditTodo
