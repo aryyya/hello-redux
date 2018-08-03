85038aab845a5127b690cece1d0aec84d86d90c0
aryyya <arya@live.ca>
2018-08-03 16:59:40 -0700

    Add styling similar to add-todo. Must be cleaned up soon.

--- client/src/components/edit-todo/edit-todo.js
+++ client/src/components/edit-todo/edit-todo.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import './edit-todo.css'
 import PropTypes from 'prop-types'
-import { withRouter } from 'react-router-dom'
+import { Link, withRouter } from 'react-router-dom'
 import { connect } from 'react-redux'
 import { todosActions } from '../../redux/todos'
 
@@ -26,59 +26,100 @@ class EditTodo extends Component {
   }
   render () {
     return (
-      <form
-        className="edit-todo"
-        onSubmit={this.onSubmit.bind(this)}
-      >
-        <p>You are at: {this.props.match.params.id}</p>
-        <input
-          type="text"
-          value={this.state.text}
-          onChange={event => this.setState({ text: event.target.value })}
-          autoFocus
-        />
-        <br />
-        low: 
-        <input
-          type="radio"
-          name="priority"
-          id="low"
-          checked={this.state.priority === 'low'}
-          onChange={() => this.setState({ priority: 'low' })}
-        />
-        <br />
-        medium: 
-        <input
-          type="radio"
-          name="priority"
-          id="medium"
-          checked={this.state.priority === 'medium'}
-          onChange={() => this.setState({ priority: 'medium' })}
-        />
-        <br />
-        high: 
-        <input
-          type="radio"
-          name="priority"
-          id="high"
-          checked={this.state.priority === 'high'}
-          onChange={() => this.setState({ priority: 'high' })}
-        />
-        <br />
-        completed: 
-        <input
-          type="checkbox"
-          name="completed"
-          id="completed"
-          checked={this.state.completed}
-          onChange={() => this.setState({ completed: !this.state.completed })}
-        />
-        <br />
-        <input
-          type="submit"
-          value="edit"
-        />
-      </form>
+      <div className="edit-todo">
+        <form
+          className="edit-todo__form"
+          onSubmit={this.onSubmit.bind(this)}
+        >
+          <div>
+            <div>
+              <Link
+                className="edit-todo__close"
+                to="/"
+              >
+                <img
+                  className="edit-todo__close-icon"
+                  src="../close-icon.svg"
+                  alt="Close."
+                />
+              </Link>
+            </div>
+            <div>
+              <input
+                className="edit-todo__text"
+                type="text"
+                value={this.state.text}
+                onChange={event => this.setState({ text: event.target.value })}
+                autoFocus
+              />
+            </div>
+            <div className="edit-todo__priorities">
+              <div>
+                <input
+                  className="edit-todo__priority edit-todo__priority--low"
+                  id="low-priority"
+                  type="radio"
+                  name="priority"
+                  checked={this.state.priority === 'low'}
+                  readOnly
+                  onChange={() => this.setState({ priority: 'low' })}
+                />
+                <label htmlFor="low-priority">
+                  Low priority
+                </label>
+              </div>
+              <div>
+                <input
+                  className="edit-todo__priority edit-todo__priority--medium"
+                  id="medium-priority"
+                  type="radio"
+                  name="priority"
+                  checked={this.state.priority === 'low'}
+                  readOnly
+                  onChange={() => this.setState({ priority: 'medium' })}
+                />
+                <label htmlFor="medium-priority">
+                  Medium priority
+                </label>
+              </div>
+              <div>
+                <input
+                  className="edit-todo__priority edit-todo__priority--high"
+                  id="high-priority"
+                  type="radio"
+                  name="priority"
+                  checked={this.state.priority === 'high'}
+                  readOnly
+                  onChange={() => this.setState({ priority: 'high' })}
+                />
+                <label htmlFor="high-priority">
+                  High priority
+                </label>
+              </div>
+            </div>
+            <div>
+              <input
+                className="edit-todo__completed"
+                type="checkbox"
+                name="completed"
+                id="completed"
+                checked={this.state.completed}
+                onChange={() => this.setState({ completed: !this.state.completed })}
+              />
+              <label htmlFor="completed">
+                Completed
+              </label>
+            </div>
+          </div>
+          <div className="edit-todo__buttons">
+            <input
+              className="edit-todo__edit-button"
+              type="submit"
+              value="Update Todo"
+            />
+        </div>
+        </form>
+      </div>
     )
   }
 }
