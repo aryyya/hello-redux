08bb0cb3fb57c97b9ee89ffa4a4c0dbb8bfe75fe
aryyya <arya@live.ca>
2018-08-02 21:27:56 -0700

    Remove toggable settings panel (in favor of edit screen).

--- client/src/components/todo-item/todo-item.js
+++ client/src/components/todo-item/todo-item.js
@@ -4,14 +4,13 @@ import { connect } from 'react-redux'
 import './todo-item.css'
 import Checkbox from '../checkbox/checkbox'
 import Priority from '../priority/priority'
-import TodoItemPanel from '../todo-item-panel/todo-item-panel'
 import { todosActions } from '../../redux/todos'
 import { withRouter } from 'react-router-dom'
 
 const TodoItem = props => {
   return (
     <li
-      className={`todo-item ${props.todo.completed ? 'todo-item--completed' : ''} ${props.todo.id === props.selectedTodoId ? 'todo-item--selected' : ''}`}
+      className={`todo-item ${props.todo.completed ? 'todo-item--completed' : ''}`}
       onClick={() => props.history.push(`/edit-todo/${props.todo.id}`)}
     >
       <div className="todo-item__top">
@@ -22,25 +21,12 @@ const TodoItem = props => {
           <span className={`todo-item__text todo-item__text--${props.todo.priority}`}>{props.todo.text}</span>
         </div>
         <div className="todo-item__buttons">
-          <img
-            className="todo-item__settings icon icon--subtle"
-            src="hammer-icon.svg"
-            alt="Change todo item settings."
-            onClick={() => props.selectTodo(props.todo.id)}
-          />
           <Checkbox
             checked={props.todo.completed}
             onClick={() => props.toggleTodo(props.todo.id)}
           />
         </div>
       </div>
-      {
-        props.selectedTodoId === props.todo.id
-          ? <TodoItemPanel
-              todo={props.todo}
-            />
-          : null
-      }
     </li>
   )
 }
@@ -48,25 +34,16 @@ const TodoItem = props => {
 TodoItem.propTypes = {
   todo: PropTypes.object.isRequired,
   toggleTodo: PropTypes.func.isRequired,
-  selectTodo: PropTypes.func.isRequired,
-  selectedTodoId: PropTypes.string.isRequired,
   history: PropTypes.object.isRequired
-}
-
-const mapStateToProps = state => {
-  return {
-    selectedTodoId: state.todosReducer.selectedTodoId
-  }
 }
 
 const mapDispatchToProps = dispatch => {
   return {
-    toggleTodo: id => dispatch(todosActions.toggleTodo(id)),
-    selectTodo: id => dispatch(todosActions.selectTodo(id))
+    toggleTodo: id => dispatch(todosActions.toggleTodo(id))
   }
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(
+export default connect(null, mapDispatchToProps)(
   withRouter(
     TodoItem
   )
