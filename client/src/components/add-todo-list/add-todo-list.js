5b3fd29d0dd0fce1dd23ef38e6f5c735132d5f1f
aryyya <arya@live.ca>
2018-08-10 22:15:40 -0700

    Start add todo component.

---
+++ client/src/components/add-todo-list/add-todo-list.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react'
+import './add-todo-list.css'
+
+class AddTodoList extends Component {
+
+  constructor (props) {
+    super(props)
+  }
+
+  render () {
+    return (
+      <form className="add-todo-list">
+        <div className="add-todo-list__section">
+          <input
+            type="text"
+            placeholder="Enter list name here."
+          />
+        </div>
+        <div className="add-todo-list__section">
+          <input
+            type="submit"
+            value="Add Todo List"
+          />
+        </div>
+      </form>
+    )
+  }
+}
