d26941eefc9bdb18f77bb1d6d1515573499585b5
aryyya <arya@live.ca>
2018-08-13 22:31:24 -0700

    Refactor for style, remove unnecessary functionality.

--- client/src/components/todo-item/todo-item.js
+++ client/src/components/todo-item/todo-item.js
@@ -1,49 +1,38 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import { connect } from 'react-redux'
+import React, { Component } from 'react'
 import './todo-item.css'
+import PropTypes from 'prop-types'
 import Checkbox from '../checkbox/checkbox'
 import Priority from '../priority/priority'
-import { todosActions } from '../../redux/todo-items'
 import { withRouter } from 'react-router-dom'
 
-const TodoItem = props => {
-  return (
-    <li
-      className={`todo-item ${props.todo.completed ? 'todo-item--completed' : ''}`}
-      onClick={() => props.history.push(`/todo-list/${props.match.params.todoListId}/edit-todo-item/${props.todo.id}`)}
-    >
-      <div className="todo-item__top">
-        <div className="todo-item__text-wrapper">
-          <Priority
-            todo={props.todo}
-          />
-          <span className={`todo-item__text todo-item__text--${props.todo.priority}`}>{props.todo.text}</span>
-        </div>
-        <div className="todo-item__buttons">
-          <Checkbox
-            todo={props.todo}
-          />
-        </div>
-      </div>
-    </li>
-  )
-}
+class TodoItem extends Component {
 
-TodoItem.propTypes = {
-  todo: PropTypes.object.isRequired,
-  toggleTodoItem: PropTypes.func.isRequired,
-  history: PropTypes.object.isRequired
-}
+  static propTypes = {
+    todo: PropTypes.object.isRequired,
+    history: PropTypes.object.isRequired
+  }
 
-const mapDispatchToProps = dispatch => {
-  return {
-    toggleTodoItem: id => dispatch(todosActions.toggleTodoItem(id))
+  render () {
+    const { history, todo } = this.props
+    const { todoListId } = this.props.match.params
+  
+    return (
+      <li
+        className={`todo-item ${todo.completed ? 'todo-item--completed' : ''}`}
+        onClick={() => history.push(`/todo-list/${todoListId}/edit-todo-item/${todo.id}`)}
+      >
+        <div className="todo-item__top">
+          <div className="todo-item__text-wrapper">
+            <Priority todo={todo} />
+            <span className={`todo-item__text todo-item__text--${todo.priority}`}>{todo.text}</span>
+          </div>
+          <div className="todo-item__buttons">
+            <Checkbox todo={todo} />
+          </div>
+        </div>
+      </li>
+    )    
   }
 }
 
-export default connect(null, mapDispatchToProps)(
-  withRouter(
-    TodoItem
-  )
-)
+export default withRouter(TodoItem)
