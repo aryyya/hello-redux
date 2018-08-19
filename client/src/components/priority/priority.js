7359a8a173a27502d3cca21ebf746ba70173a8f5
aryyya <arya@live.ca>
2018-08-18 20:09:47 -0700

    Put component styling at the bottom.

--- client/src/components/priority/priority.js
+++ client/src/components/priority/priority.js
@@ -2,19 +2,6 @@ import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import styled from 'styled-components'
 
-const getDotBackgroundColor = priority => ({
-  'low': '#47e4c2',
-  'medium': '#fcc300',
-  'high': '#ff88a6'
-}[priority])
-
-const StyledPriority = styled.div`
-  width: 9px;
-  height: 9px;
-  border-radius: 50%;
-  background-color: ${({ priority }) => getDotBackgroundColor(priority)};
-`
-
 class Priority extends Component {
 
   static propTypes = {
@@ -29,5 +16,18 @@ class Priority extends Component {
     )
   }
 }
+
+const getDotBackgroundColor = priority => ({
+  'low': '#47e4c2',
+  'medium': '#fcc300',
+  'high': '#ff88a6'
+}[priority])
+
+const StyledPriority = styled.div`
+  width: 9px;
+  height: 9px;
+  border-radius: 50%;
+  background-color: ${({ priority }) => getDotBackgroundColor(priority)};
+`
 
 export default Priority
