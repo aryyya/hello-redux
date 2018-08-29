6aea8b554500ca33457c91e2c8c3a36b8222ebc5
aryyya <arya@live.ca>
2018-08-28 20:32:18 -0700

    Show incomplete todo items.

--- client/src/components/all-todo-items/all-todo-items.js
+++ client/src/components/all-todo-items/all-todo-items.js
@@ -1,19 +1,41 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
+import { connect } from 'react-redux'
 import styled from 'styled-components'
+import TodoItem from '../todo-item/todo-item'
+import getArrayFromMap from '../../utility/get-array-from-map';
 
 class AllTodoItems extends Component {
 
+  static propTypes = {
+    incompleteTodoItems: PropTypes.array.isRequired
+  }
+
   render () {
+    const { incompleteTodoItems } = this.props
+
     return (
       <StyledAllTodoItems>
         AllTodoItems component
+        <StyledAllTodoItemsTodoItems>
+          {incompleteTodoItems.map(todoItem => <TodoItem todo={todoItem} key={todoItem.id} />)}
+        </StyledAllTodoItemsTodoItems>
       </StyledAllTodoItems>
     )
   }
 }
 
-export default AllTodoItems
+const mapStateToProps = state => {
+  const todoItems = getArrayFromMap(state.todoItemsReducer.todoItems)
+  const incompleteTodoItems = todoItems.filter(todoItem => !todoItem.completed)
 
-const StyledAllTodoItems = styled.div`
-`
+  return {
+    incompleteTodoItems
+  }
+}
+
+export default connect(mapStateToProps, null)(AllTodoItems)
+
+const StyledAllTodoItems = styled.div``
+
+const StyledAllTodoItemsTodoItems = styled.ul``
