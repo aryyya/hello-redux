961ef1247cf2a74102f484528aaefed73ef6796a
aryyya <arya@live.ca>
2018-09-02 01:14:52 -0700

    Add flex direction properties.

--- client/src/components/page/page.js
+++ client/src/components/page/page.js
@@ -3,19 +3,23 @@ import styled from 'styled-components'
 const Page = styled.div`
   height: 100%;
   display: flex;
-  flex-direction: column;
+  flex-direction: ${({ horizontal, vertical }) =>
+    vertical ? 'column' : horizontal ? 'row' : 'column'
+  };
 `
 
 const PageSection = styled.div`
-  flex: ${({flex}) =>
+  display: flex;
+  flex: ${({ flex }) =>
     flex || '1'
   };
-  display: flex;
-  flex-direction: column;
+  flex-direction: ${({ horizontal, vertical }) =>
+    vertical ? 'column' : horizontal ? 'row' : 'auto'
+  };
   align-items: center;
   justify-content: center;
   padding: 3.5rem;
-`
+  `
 
 export {
   Page,
